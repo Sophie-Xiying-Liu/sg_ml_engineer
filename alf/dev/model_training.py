6f7f3eac97e7d6e6abb230cb24c7e0f68df14529
liuxixi <xiyliu@gmail.com>
2023-12-16 17:48:09 +0100

    thoughts on ml engineering doc

--- alf/dev/model_training.py
+++ alf/dev/model_training.py
@@ -10,12 +10,16 @@ import numpy as np
 import yaml
 import logging
 from pickle import load, dump
-import lightgbm as lgb
-from sklearn.model_selection import train_test_split
+import lightgbm as lgbm
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import mean_absolute_error
 from sklearn.metrics import r2_score
-
+from sklearn.metrics import mean_absolute_percentage_error
+from sklearn.model_selection import TimeSeriesSplit
+from sklearn.multioutput import RegressorChain
+import optuna
+from optuna.samplers import TPESampler
+from optuna.integration import LightGBMTuner
 
 
 # Set the current working directory to the directory of this file
@@ -88,39 +92,90 @@ def create_sets(df, dt_col, target_col, train_start, train_end):
     X_test = df_test[feature_cols]
     y_test = df_test[target_cols]
 
+    print(f"X_train shape: {X_train.shape}")
+    print(f"y_train shape: {y_train.shape}")
+    print(f"X_test shape: {X_test.shape}")
+    print(f"y_test shape: {y_test.shape}")
+
     return X_train, y_train, X_test, y_test
 
-def fe_transform(X_train, preprocessor_file):
+def fe_transform(X_train, X_test, preprocessor_file):
     """FE fit transform"""
     # Fit preprocess on X_train
     preprocessor_file_path = os.path.join(os.path.dirname(__file__), preprocessor_file)
     preprocessor = load(open(preprocessor_file_path, 'rb'))
     fitted_preprocessor = preprocessor.fit(X_train)
 
-    X_train = pd.DataFrame(
+    X_train_transformed = pd.DataFrame(
             fitted_preprocessor.transform(X_train),
             columns=fitted_preprocessor.get_feature_names_out(),
         )
+    print(f"X_train_transformed shape: {X_train_transformed.shape}")
 
-    X_test = pd.DataFrame(
+    X_test_transformed = pd.DataFrame(
         fitted_preprocessor.transform(X_test),
         columns=fitted_preprocessor.get_feature_names_out(),
     )
+    print(f"X_test_transformed shape: {X_test_transformed.shape}")
 
-    return X_train, X_test
+    return X_train_transformed, X_test_transformed
 
-def train_model(X_train, y_train, X_val, y_val, model, model_name):
+def train_model(lgbm_params, X_train_transformed, y_train, X_test_transformed, y_test, model_name):
     """Train model"""
 
+    # define the lgbm objective fct for Optuna tuning
+    def objective(trial):
+        params = {
+            "objective": "regression",
+            "metric": "rmse",
+            "boosting_type": "gbdt",
+            "n_estimators": trial.suggest_int("n_estimators", 7000, 9000, step=50),
+            "learning_rate": trial.suggest_float("learning_rate", 0.04, 0.07),
+            "num_leaves": trial.suggest_int("num_leaves", 44, 50),
+            "min_child_samples": trial.suggest_int("min_child_samples", 25, 30),
+            "min_child_weight": trial.suggest_float("min_child_weight", 0.00045, 0.00055),
+            "subsample": trial.suggest_float("subsample", 0.60, 0.70),
+            "colsample_bytree": trial.suggest_float("colsample_bytree", 0.7, 0.9),
+            "reg_alpha": trial.suggest_float("reg_alpha", 0.9, 1.0),
+        }
 
-    model.fit(X_train, y_train)
-    y_pred = model.predict(X_val)
+        metrics = []
 
-    model_path = os.path.join(os.path.dirname(__file__), f"models/{model_name}.pickle")
-    with open(model_path, 'wb') as file:
-        dump(model, file)
+        tss = TimeSeriesSplit(n_splits=3)
+        for train_index, val_index in tss.split(X_train_transformed):
+            X_train_cv, X_val_cv = X_train_transformed.iloc[train_index], X_train_transformed.iloc[val_index]
+            y_train_cv, y_val_cv = y_train.iloc[train_index], y_train.iloc[val_index]
 
-    return y_pred
+            print(X_train_cv.shape, X_val_cv.shape, y_train_cv.shape, y_val_cv.shape)
+
+            lgbm_reg = lgbm.LGBMRegressor(**lgbm_params)
+            model = RegressorChain(
+                base_estimator=lgbm_reg,
+                order=[i for i in range(0, 24)],
+            )
+            model.fit(X_train_cv, y_train_cv)
+
+            y_val_pred = model.predict(X_val_cv)
+
+            rmse = np.sqrt(mean_squared_error(y_val_cv.values.flatten(), y_val_pred.flatten()))
+            mae = mean_absolute_error(y_val_cv.values.flatten(), y_val_pred.flatten())
+            r2 = r2_score(y_val_cv.values.flatten(), y_val_pred.flatten())
+            mape = mean_absolute_percentage_error(y_val_cv.values.flatten(), y_val_pred.flatten())
+
+
+            print(f"RMSE: {rmse}")
+            print(f"MAE: {mae}")
+            print(f"R2: {r2}")
+            print(f"MAPE: {mape}")
+        
+        model_path = os.path.join(os.path.dirname(__file__), f"models/{model_name}.pickle")
+        with open(model_path, 'wb') as file:
+            dump(model, file)
+        print("model saved")
+
+        break
+
+    return model, rmse, mae, r2, mape
 
 
 if __name__ == "__main__":
@@ -132,6 +187,8 @@ if __name__ == "__main__":
     TRAIN_START = params["train_start"]
     TRAIN_END = params["train_end"]
     PREPROCESSOR_FILE = params["preprocessor_file"]
+    N_SPLITS = params["n_splits"]
+    LGBM_PARAMS = params["lgbm_params"]
 
 
     df = import_fe_file(
@@ -154,14 +211,26 @@ if __name__ == "__main__":
         train_end=TRAIN_END,
     )
     print("train test split created")
-    # X_train_transformed, X_test_transformed = fe_transform(
-    #     X_train,
-    #     preprocessor_file=PREPROCESSOR_FILE,
-    # )
+    X_train_transformed, X_test_transformed = fe_transform(
+        X_train,
+        X_test,
+        preprocessor_file=PREPROCESSOR_FILE,
+    )
 
-    print(df.head())
-    print(df.tail())
-    print(df.shape)
-    print(df.info())
-    print(df.describe())
-    print(df.columns.to_list())
+    model, rmse, mae, r2, mape = (
+        train_model(
+            lgbm_params=LGBM_PARAMS,
+            X_train_transformed=X_train_transformed,
+            y_train=y_train,
+            X_test_transformed=X_test_transformed,
+            y_test=y_test,
+            model_name='test_model_2')
+    )
+    
+
+    # print(df.head())
+    # print(df.tail())
+    # print(df.shape)
+    # print(df.info())
+    # print(df.describe())
+    # print(df.columns.to_list())
--- docs/thoughts_ml_engineering.md
+++ docs/thoughts_ml_engineering.md
@@ -53,7 +53,9 @@ Below are the points to be decided:
 
 - Monitoring and logging: 
     - Log prediction requests -> alarm when important tasks fail + BCM
+
     - Track performance -> alarm when performance drops below the (pre-defined) criterium
+
     - Track health of the serving infrastructure
         (response time, resource utilisation, availability & uptime, errors, error rates, success rates of requests, overall availability of interfaces)
 
@@ -63,3 +65,53 @@ Below are the points to be decided:
 
 ## ML Phase 2: First Pipeline
 
+It is recommended to build up the first stable and reliable pipeline first, leave the fancy features out for future iteration. But it doesn't mean the first pipeline should be built quick and dirty, rather simple, correct, and reliable. Especially when considering scalability, compatibility, and serving infrastructure, the methods used to build up the first pipeline should be chosen wisely. For example, for data scientists to build up a PoC, Pandas can be an effective toll for its simplicity and easy of use and rich functionality, but it will introduce speed and performance limitation when the dataset-size increases and cannot not fit into memory. To avoid re-factor the whole data processing pipeline, it is more efficient to use other methods such as PySpark.
+
+### #5 Simple, accurate, reliable 1st Pipeline
+
+The criteria below can help decide if it is good enough as the first complete pipeline:
+
+1. How to get examples to your learning algorithm.
+2. A first cut as to what “good” and “bad” mean to your system.
+3. How to integrate your model into your application.
+
+Choosing simple features makes it easier to ensure that:
+1. The features reach your learning algorithm correctly.
+2. The model learns reasonable weights.
+3. The features reach your model in the server correctly.
+Once you have a system that does these three things reliably, you have done most of the work. Your simple model provides you with baseline metrics and a baseline behavior that you can use to test more complex models. Some teams aim for a “neutral” first launch: a first launch that explicitly de­prioritizes machine learning gains, to avoid getting distracted.
+
+These advice I get the advice from the Google doc directly, as there is no adjustment or improvment I can add.
+
+### #6 Testing infrastructure independently from ML
+
+1. Testing all the data pipeline before it reaches the model (check statistics for data quality control)
+
+2. Testing models to avoid train-serve skew
+
+Reasons can potentially cause the skew:
+
+- discrepancy between data pipelines in training and serving
+- change in data
+
+Possible solutions can be:
+
+- Explicit monitoring 
+- Measure training-serving skew on training data vs. holdout data and on holdout data vs. “next-day” data
+- Log features at serving time and use them at training time
+- Re-use code between training pipeline and serving pipeline
+
+For example, a same set of data should generate the same results in both train and serving environment.
+
+## ML Phase 2.5: Continuous Improvement of ML Models
+
+### #7 Examine the end user experience
+
+Do it especially when integrating the model in their operational systems.
+
+### # 8 Measure the performance differences between models
+
+Look for patterns in the measured errors which are outside of the current feature set, and create new features. 
+
+
+This is far away from completion but I will take a break given the time constrains, and also because I have less experience in the deployment phase, thus this part is expected to be added at a later stage. 
